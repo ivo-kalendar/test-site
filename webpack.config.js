3c694dd874ada76a6364c4ca7ee7dd3e30805518
Ivo <ivokalendar@icloud.com>
2019-12-27 12:10:27 +0100

    Working on Mobile Menu

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+
+let path = require('path')
+
+
+module.exports = {
+	mode: "development",
+	entry: "./app/assets/scripts/App.js",
+	output: {
+		path: path.resolve(__dirname, "./app/temp/scripts"),
+		filename: "App.js"
+	},
+	module: {
+		rules: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				},
+				test: /\.js$/,
+				exclude: /node_modules/
+			}
+		]
+	}
+}
